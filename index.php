bce206c606da997d811fe0acd6b14e48f9a213bd
J P Edirisinghe <37530779+janaka531@users.noreply.github.com>
2019-04-08 16:17:11 +0530

    Update index.php

--- index.php
+++ index.php
@@ -91,7 +91,7 @@ if(isset($_POST['submit']) ) {
 			}
 			?>
 			<div class="qr-field">
-				<h2 style="text-align:center">QR Code Result: </h2>
+				<h2 style="text-align:center">QR Code Result:</h2>
 				<center>
 					<div class="qrframe" style="border:2px solid black; width:210px; height:210px;">
 							<?php echo '<img src="temp/'. @$filename.'.png" style="width:200px; height:200px;"><br>'; ?>
